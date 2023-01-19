b59dd264207bf049dac4a4a4e639263b1dd1c301
Erik Gonzaga <erikgonzaga@outlook.com>
2023-01-19 08:16:59 -0300

    Repository de Endereço e Query para buscar pessoa pelo id no Banco.

---
+++ src/main/java/com/gonzaga/gestaopessoas/repository/EnderecoRepository.java
@@ -0,0 +1,14 @@
+package com.gonzaga.gestaopessoas.repository;
+
+import com.gonzaga.gestaopessoas.model.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Long>{
+
+    List<Endereco> findAllByPessoaId(Long idPessoa);
+
+}
