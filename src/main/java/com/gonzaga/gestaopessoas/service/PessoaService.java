d78d8106963aac388033df3de0b690857300f8ec
Erik Gonzaga <erikgonzaga@outlook.com>
2023-01-19 03:26:36 -0300

    Regras de Négocio.

---
+++ src/main/java/com/gonzaga/gestaopessoas/service/PessoaService.java
@@ -0,0 +1,14 @@
+package com.gonzaga.gestaopessoas.service;
+
+import com.gonzaga.gestaopessoas.model.Pessoa;
+
+import java.util.List;
+
+public interface PessoaService {
+
+    Pessoa criar(Pessoa pessoa);
+    boolean editar(Long id, String nome, String nascimento);
+    Pessoa consultar(Long id);
+    List<Pessoa> listar();
+
+}
