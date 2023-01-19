f6e6ba4aa8dd75a06aed66fc742408ff16f2de9b
Erik Gonzaga <erikgonzaga@outlook.com>
2023-01-19 19:05:24 -0300

    Parametro Nascimento para tipo LocalDate

--- src/main/java/com/gonzaga/gestaopessoas/controller/GestaoPessoaController.java
+++ src/main/java/com/gonzaga/gestaopessoas/controller/GestaoPessoaController.java
@@ -9,6 +9,7 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 
