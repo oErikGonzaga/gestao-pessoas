cc20fe14955f1ab3480e68e7beb59f675b238440
Erik Gonzaga <erikgonzaga@outlook.com>
2023-01-18 22:01:53 -0300

    Iniciando Projeto

---
+++ src/main/java/com/gonzaga/gestaopessoas/GestaopessoasApplication.java
@@ -0,0 +1,13 @@
+package com.gonzaga.gestaopessoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaopessoasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaopessoasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gonzaga/gestaopessoas/GestaopessoasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gonzaga.gestaopessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaopessoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
