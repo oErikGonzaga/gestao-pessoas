35b33e45c4e347e2096b8532989142778ea90d95
Erik Gonzaga <erikgonzaga@outlook.com>
2023-01-19 08:23:05 -0300

    Update

--- src/main/java/com/gonzaga/gestaopessoas/repository/PessoaRepository.java
+++ src/main/java/com/gonzaga/gestaopessoas/repository/PessoaRepository.java
@@ -4,13 +4,7 @@ import com.gonzaga.gestaopessoas.model.Pessoa;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
-
 @Repository
 public interface PessoaRepository extends JpaRepository<Pessoa, Long>{
-
-    Pessoa findFirstByNome(String nome);
-    Pessoa findFirstByNascimento(LocalDate nascimento);
-
 
 }
