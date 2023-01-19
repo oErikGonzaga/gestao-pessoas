4a1a03b474a36de2798c4932ba9bb67fe8ed04be
Erik Gonzaga <erikgonzaga@outlook.com>
2023-01-19 19:03:34 -0300

    Gerendo ID em ordem crescente de endereço.

--- src/main/java/com/gonzaga/gestaopessoas/model/Endereco.java
+++ src/main/java/com/gonzaga/gestaopessoas/model/Endereco.java
@@ -13,7 +13,7 @@ import javax.persistence.*;
 public class Endereco {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
