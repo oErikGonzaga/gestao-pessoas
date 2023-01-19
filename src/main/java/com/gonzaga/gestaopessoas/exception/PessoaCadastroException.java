1fff694ce825a84e8efbf2b16e0125017c3e7c1b
Erik Gonzaga <erikgonzaga@outlook.com>
2023-01-19 08:22:40 -0300

    Exception de Cadastro de Pessoa.

---
+++ src/main/java/com/gonzaga/gestaopessoas/exception/PessoaCadastroException.java
@@ -0,0 +1,8 @@
+package com.gonzaga.gestaopessoas.exception;
+
+public class PessoaCadastroException extends RuntimeException{
+
+    public PessoaCadastroException(String mensagem){
+        super(mensagem);
+    }
+}
