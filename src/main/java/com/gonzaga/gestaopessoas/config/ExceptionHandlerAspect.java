4f8430ca6d65491837e4a221251a9fc832a2ead7
Erik Gonzaga <erikgonzaga@outlook.com>
2023-01-19 08:20:57 -0300

    ExceptionHandlerAspect de Cadastro de Pessoa e Endereço.

--- src/main/java/com/gonzaga/gestaopessoas/config/ExceptionHandlerAspect.java
+++ src/main/java/com/gonzaga/gestaopessoas/config/ExceptionHandlerAspect.java
@@ -1,5 +1,7 @@
 package com.gonzaga.gestaopessoas.config;
 
+import com.gonzaga.gestaopessoas.exception.EnderecoCadastroException;
+import com.gonzaga.gestaopessoas.exception.PessoaCadastroException;
 import com.gonzaga.gestaopessoas.exception.PessoaNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,8 +10,17 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class ExceptionHandlerAspect {
+
     @ExceptionHandler(PessoaNotFoundException.class)
     public final ResponseEntity<String> handle(PessoaNotFoundException exception){
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Pessoa não encontrada");
+    }
+    @ExceptionHandler(PessoaCadastroException.class)
+    public final ResponseEntity<String> handle(PessoaCadastroException exception){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
+    }
+    @ExceptionHandler(EnderecoCadastroException.class)
+    public final ResponseEntity<String> handle(EnderecoCadastroException exception){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
     }
 }
