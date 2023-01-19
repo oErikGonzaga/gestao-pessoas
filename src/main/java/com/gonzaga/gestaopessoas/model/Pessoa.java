59b941e2c0905ab44d30083333a17d886562a605
Erik Gonzaga <erikgonzaga@outlook.com>
2023-01-19 03:19:56 -0300

    Criação de classe Pessoa, marcando com @Entity, @Table para referenciar nome da tabela no banco.

---
+++ src/main/java/com/gonzaga/gestaopessoas/model/Pessoa.java
@@ -0,0 +1,24 @@
+package com.gonzaga.gestaopessoas.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "pessoas")
+public class Pessoa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(length = 200, nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private LocalDate nascimento;
+}
