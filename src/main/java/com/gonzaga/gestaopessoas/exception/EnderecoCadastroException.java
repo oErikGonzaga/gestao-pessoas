667ebedf9d3674a872cb551df2e845a15e2accb6
Erik Gonzaga <erikgonzaga@outlook.com>
2023-01-19 08:15:32 -0300

    Exception para cadastro de Endereço.

---
+++ src/main/java/com/gonzaga/gestaopessoas/exception/EnderecoCadastroException.java
@@ -0,0 +1,8 @@
+package com.gonzaga.gestaopessoas.exception;
+
+public class EnderecoCadastroException extends RuntimeException{
+
+    public EnderecoCadastroException(String mensagem){
+        super(mensagem);
+    }
+}
